12b383d2aff1a1959b90e2163c004987495b40e7
Soundar <soundar.kpr@gmail.com>
2026-05-26 01:16:23 +0530

    bubble.js - code cleanup

--- src/Chart/bubble.js
+++ src/Chart/bubble.js
@@ -3,22 +3,7 @@ import { convertRange, unitValue } from '../Base/util';
 const BACKGROUND = '--background-';
 const defaultBubbleRadius = 10;
 
-export default function Bubble(
-    {
-        points,
-        bubbleColor,
-        bubbleBorderColor,
-        bubbleBorderWidth,
-        bubbleOpacity
-    },
-    {
-        yMin,
-        yMax,
-        chartHeight,
-        chartWidth,
-        startPosition
-    }
-) {
+export default function Bubble({ points, bubbleOpacity }, { yMin, yMax, chartHeight, chartWidth, startPosition }) {
     var backgroundImage = [],
         backgroundSize = [],
         backgroundPosition = [],
@@ -28,9 +13,6 @@ export default function Bubble(
     // later this should come from graph.js
     var xMin = 0;
     var xMax = 4;
-
-    // usable graph width
-    // var chartWidth = chartHeight;
 
     points.forEach(function(point, index) {
         var xValue, yValue, radius;
@@ -49,63 +31,21 @@ export default function Bubble(
             radius = defaultBubbleRadius;
         }
 
-        // convert x coordinate
-        var bubbleX = convertRange(
-            xValue,
-            xMin,
-            xMax,
-            0,
-            chartWidth
-        );
-
-        // convert y coordinate
-        var bubbleY = convertRange(
-            yValue,
-            yMin,
-            yMax,
-            0,
-            chartHeight
-        );
-
-        if (!(bubbleX >= 0)) {
-            bubbleX = 0;
-        }
-
-        if (!(bubbleY >= 0)) {
-            bubbleY = 0;
-        }
-
         var diameter = radius * 2;
+        // convert x, y coordinate
+        var bubbleX = convertRange(xValue, xMin, xMax, 0, chartWidth);
+        var bubbleY = convertRange(yValue, yMin, yMax, 0, chartHeight);
+
+        if (!(bubbleX >= 0)) bubbleX = 0;
+        if (!(bubbleY >= 0)) bubbleY = 0;
 
         // final render positions
         var posX = startPosition + bubbleX - radius;
-
         var posY = chartHeight - bubbleY - radius;
 
-        // bubble style
-        var getBubbleImage = function() {
-            // return `
-            //     radial-gradient(
-            //         circle,
-            //         ${bubbleColor} 62%,
-            //         ${bubbleBorderColor} 62%,
-            //         ${bubbleBorderColor} calc(62% + ${bubbleBorderWidth}),
-            //         transparent calc(62% + ${bubbleBorderWidth})
-            //     )
-            // `;
-
-            return 'var(--bubble)';
-        };
-
-        backgroundImage.push(getBubbleImage());
-
-        backgroundSize.push(
-            unitValue(diameter) + ' ' + unitValue(diameter)
-        );
-
-        backgroundPosition.push(
-            unitValue(posX) + ' ' + unitValue(posY)
-        );
+        backgroundImage.push('var(--bubble)');
+        backgroundSize.push(unitValue(diameter) + ' ' + unitValue(diameter));
+        backgroundPosition.push(unitValue(posX) + ' ' + unitValue(posY));
     });
 
     styles[BACKGROUND + 'image'] = backgroundImage.join(', ');
